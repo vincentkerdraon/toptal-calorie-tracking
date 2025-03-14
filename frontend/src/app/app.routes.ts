e5d391995eff8bd8c299eb81f0de38b7efa4a788
Vincent KERDRAON <vincent.kerdraon@gmail.com>
2025-03-14 14:29:20 -0600

    front; create router

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './views/dashboard/dashboard.component';
+import { LoginComponent } from './views/login/login.component';
+import { ReportingComponent } from './views/reporting/reporting.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'admin/reporting', component: ReportingComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' }, // Default route
+];
---
+++ frontend/src/app/views/reporting/reporting.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reporting',
+  imports: [],
+  templateUrl: './reporting.component.html',
+  styleUrl: './reporting.component.scss'
+})
+export class ReportingComponent {
+
+}
